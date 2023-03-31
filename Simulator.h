d861485c40b566f9260222b4031add550913957f
FreemanTheMaverick <63384635+FreemanTheMaverick@users.noreply.github.com>
2023-03-31 16:14:07 +0800

    Delete Simulator.h

--- Simulator.h
+++
@@ -1,3 +0,0 @@
-void Simulator_writeHeaders(int nmcs,std::string * mc_labels);
-
-void Simulator_runEuler(double stepsize,long int nsteps,long int print,int nmcs,int ntss,double * mc_concentrations,int * ts_nsreactants,int * ts_reactants,int * ts_nsproducts,int * ts_products,int * mc_nstss,double * ts_frcs,double * ts_brcs,bool output);
